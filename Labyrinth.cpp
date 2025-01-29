21ae6d5957e55ca724f8640360356c44c0445c36
Radamir <dermeister20032132134@gmail.com>
2025-01-29 18:55:07 +0300

    Начал реализацию лабиринта

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Labyrinth.h"
+bool Labyrinth::isBelongsToLabyrinth(int x, int y) const {
+    spdlog::info("Entrypoint: Labyrinth::bool isBelongsToLabyrinth(int x, int y) const");
+    if (x<0
+        || y <0
+        || x > LABYRINTH_WIDTH + FRAME_THICKNESS-1
+        || y > LABYRINTH_HEIGHT + FRAME_THICKNESS-1) {
+        spdlog::error("OUT OF RANGE IN: Labyrinth::bool isBelongsToLabyrinth(int x, int y) const");
+        throw std::out_of_range("Labyrinth::isBelongsToLabyrinth");
+    }
+    spdlog::info("Returned: Labyrinth::bool isBelongsToLabyrinth(int x, int y) const");
+    return true;
+}
+
+bool Labyrinth::isFloor(int x, int y) const {
+    spdlog::info("Labyrinth::bool isFloor(int x, int y) const");
+
+}
+
--- Labyrinth.h
+++ Labyrinth.h
@@ -4,7 +4,9 @@
 
 #ifndef LABYRINTH_H
 #define LABYRINTH_H
-
+#include <vector>
+#include <utility>
+#include <spdlog/spdlog.h>
 //  Этот класс будет использоваться в генетическом алгоритме, так что не удивляйтесь размножению лабиринтов
 
 class Labyrinth {
@@ -18,12 +20,15 @@ public:
     [[nodiscard]] bool isPassable() const; // проверка лабиринта на проходимость
     [[nodiscard]] int score() const; // узнать счет этого лабиринта
     void setCell(int x, int y, char value); // установить указанное значение в выбранной клетке
-    Labyrinth getDescendant(Labyrinth &partner); // получить потомка 
+    Labyrinth getDescendant(Labyrinth &partner); // получить потомка
+    [[nodiscard]] bool isWall(int x, int y) const; // этот метод и метод ниже - проверка на то проходима ли клетка
+    [[nodiscard]] bool isFloor(int x, int y) const;
 private:
-    char** labyrinth;
+    std::vector<std::vector<char>> labyrinth;
     const int LABYRINTH_WIDTH = 30;
     const int LABYRINTH_HEIGHT = 20;
-    const int frameThickness = 1;
+    const int FRAME_THICKNESS = 1;
+    [[nodiscard]] bool isBelongsToLabyrinth(int x, int y) const; // проверка на то попадает ли точка в лабиринт
     ~Labyrinth();
 };
 
