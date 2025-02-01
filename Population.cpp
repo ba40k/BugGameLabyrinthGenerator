d662d348edf735476f808a95557014e95fe79ab4
Radamir <dermeister20032132134@gmail.com>
2025-02-01 17:51:33 +0300

    Доделал лабиринт, создал класс Популяция

---
+++ Population.cpp
@@ -0,0 +1,5 @@
+//
+// Created by radamir on 01.02.25.
+//
+
+#include "Population.h"
---
+++ Population.h
@@ -0,0 +1,16 @@
+//
+// Created by radamir on 01.02.25.
+//
+
+#ifndef POPULATION_H
+#define POPULATION_H
+
+
+
+class Population {
+
+};
+
+
+
+#endif //POPULATION_H
